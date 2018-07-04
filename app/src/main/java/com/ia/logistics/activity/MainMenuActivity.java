ca275665eaf59218e52421dad11a852af60cf7a8
shuxinghu <shu.xinghu@jinyinmao.com.cn>
2018-07-05 00:03:31 +0800

    回滚

--- app/src/main/java/com/ia/logistics/activity/MainMenuActivity.java
+++ app/src/main/java/com/ia/logistics/activity/MainMenuActivity.java
@@ -57,6 +57,67 @@ public class MainMenuActivity extends BaseComActivity {
 			Intent intent = null;
 			switch (v.getId()) {
 				case R.id.imageButtonmain_mybill:// 我的货
+					new AsyncSendDataTask(MainMenuActivity.this) {
+
+						@Override
+						protected String doInBackground(Object... params) {
+							// TODO Auto-generated method stub
+							if (InterfaceDates.getInstance().HavingOldTrip(mContext)) {
+								Log.d("车次状态号", MyApplications.getInstance().getTrip_state());
+								System.out.println("车次状态--"+MyApplications.getInstance().getTrip_state());
+								if (MyApplications.getInstance().getTrip_state().startsWith("0")) {
+									InterfaceDates.getInstance().LoadLastOldTrip(mContext, Constant.PackageState.PACKAGE_UPLOADED);
+									Log.d("车次状态", "已装车");
+									return "11#";
+								}
+								if ("10".equals(MyApplications.getInstance().getTrip_state())
+										|| "20".equals(MyApplications.getInstance()
+										.getTrip_state())) {
+									InterfaceDates.getInstance().LoadLastOldTrip(mContext, Constant.PackageState.PACKAGE_DEPARTED);
+									Log.d("车次状态", "已发车");
+									return "12#";
+								}
+								if ("21".equals(MyApplications.getInstance()
+										.getTrip_state())
+										|| "30".equals(MyApplications.getInstance()
+										.getTrip_state())) {
+									getSharedPreferences("localPage",
+                                            MODE_PRIVATE).edit()
+											.putString("localPage", "").commit();
+									String where = " local_status='"
+											+ Constant.PackageState.PACKAGE_DEPARTED + "'";
+									ADVT_DBHelper.getAdvtDBHelper().delete(
+											Constant.Table.TB_PACKAGE, where);
+									Log.d("车次状态", "已到货");
+									InterfaceDates.getInstance().inserBillInfo();
+									return "13#";
+								}
+								return "1#";
+							} else {
+//							CommSet.inserBillInfo();
+								return "2#";
+							}
+						}
+
+						@Override
+						protected void onPostExecute(String result) {
+							// TODO Auto-generated method stub
+							Intent intent = null;
+							if (result.startsWith("11#")) {
+								intent = new Intent(MainMenuActivity.this,DepartActivity.class);
+							}else if (result.startsWith("12#")) {
+								intent = new Intent(MainMenuActivity.this,ArrivalActivity.class);
+							}else if (result.startsWith("13#")) {
+								intent = new Intent(MainMenuActivity.this,SignActivity.class);
+							}else {
+								intent = new Intent(MainMenuActivity.this,MybillActivity.class);
+							}
+							startActivity(intent);
+							finish();
+							super.onPostExecute(result);
+						}
+
+					}.execute();
 					break;
 				case R.id.imageButtonmain_sign:
 					intent = new Intent(MainMenuActivity.this,SignActivity.class);
